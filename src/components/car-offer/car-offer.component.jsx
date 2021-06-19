0a91c3514d84252d34fdf5d105c20a7c141c1a4c
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-19 21:55:56 +0200

    STYLINGS

--- src/components/car-offer/car-offer.component.jsx
+++ src/components/car-offer/car-offer.component.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./car-offer.style.scss";
 
+function currencyFormat(number) {
+  return new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
+  }).format(number / 100);
+}
+
 export const CarOffer = ({ offer }) => (
-  <div
-    className="tour-container image"
-    style={{ backgroundImage: `url(${offer.vehicleType.images.native})` }}
-  >
-    <h2> {offer.amount} </h2>
+  <div className="offer-container">
+    <div
+      className="offer__background image"
+      style={{ backgroundImage: `url(${offer.vehicleType.images.native})` }}
+    ></div>
+    <p>{offer.vehicleType.title}</p>
+    <p>{offer.vehicleType.class}</p>
+    <p> {currencyFormat(offer.amount)}</p>
   </div>
 );
