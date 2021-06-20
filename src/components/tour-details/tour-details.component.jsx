1dbab368569587b7c6917031a6ab51c0e54732cd
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-06-20 23:42:10 +0200

    Styled details component

--- src/components/tour-details/tour-details.component.jsx
+++ src/components/tour-details/tour-details.component.jsx
@@ -3,22 +3,24 @@ import "./tour-details.style.scss";
 import moment from "moment";
 
 export const TourDetails = ({ startDate, tourLabel, duration, address }) => (
-  <div className="tour tour__details-container">
-    <span className={startDate ? "" : "red"}>
-      Date and time:{" "}
-      <strong>
-        {" "}
-        {startDate ? moment(startDate).format("DD.MM.YYYY - HH:mm") : ""}
-      </strong>
-    </span>
-    <span className={tourLabel ? "" : "red"}>
-      Selected Tour: <strong>{tourLabel} </strong>{" "}
-    </span>
-    <span className={tourLabel ? "" : "red"}>
-      Address: <strong>{address} </strong>{" "}
-    </span>
-    <span className={duration ? "" : "red"}>
-      Duration: <strong>{`${duration ? duration + "h" : ""}`}</strong>
-    </span>
+  <div className="tour__wrapper">
+    <div className="tour tour__details-container">
+      <span className={startDate ? "" : "red"}>
+        Date and time:{" "}
+        <strong>
+          {" "}
+          {startDate ? moment(startDate).format("DD.MM.YYYY - HH:mm") : ""}
+        </strong>
+      </span>
+      <span className={tourLabel ? "" : "red"}>
+        Selected Tour: <strong>{tourLabel} </strong>{" "}
+      </span>
+      <span className={tourLabel ? "" : "red"}>
+        Address: <strong>{address} </strong>{" "}
+      </span>
+      <span className={duration ? "" : "red"}>
+        Duration: <strong>{`${duration ? duration + "h" : ""}`}</strong>
+      </span>
+    </div>
   </div>
 );
